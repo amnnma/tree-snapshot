3770fcfd7622b60d8fc50ea71c0c4659324a1ea2
AUM <blackpotter14@gmail.com>
2022-02-19 22:02:02 +0700

    🔥 stage 2

--- src/config.js
+++ src/config.js
@@ -1,42 +1,41 @@
-const { REACT_APP_MAPBOX_ACCESS_TOKEN } = process.env;
-
 export default {
-  style: 'mapbox://styles/amnnma/ckzqnnobf00o314p8jlof4bc9',
-  accessToken: REACT_APP_MAPBOX_ACCESS_TOKEN,
+  style: "mapbox://styles/amnnma/ckzqnnobf00o314p8jlof4bc9",
+  accessToken:
+    "pk.eyJ1IjoiYW1ubm1hIiwiYSI6ImNrcTgycDUzaTA5aGcybnMxeGFneTFxenoifQ.uD9aFRQVwIgbKTOukVdr6Q",
   showMarkers: false,
-  theme: 'dark',
+  theme: "dark",
   intro: {
-    title: 'Test',
-    subtitle: 'Test',
+    title: "Test",
+    subtitle: "Test",
   },
-  alignment: 'left',
-  footer: 'NSC2022',
+  alignment: "left",
+  footer: "NSC2022",
   chapters: [
     {
-      id: 'amazon-region',
-      title: 'amnnma',
-      image: '',
-      source: 'asd',
+      id: "amazon-region",
+      title: "amnnma",
+      image: "",
+      source: "asd",
       description:
-        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       location: {
         center: [100.523186, 13.736],
         zoom: 4.42,
         pitch: 0.0,
-        bearing: -8.78
+        bearing: -8.78,
       },
       onChapterEnter: [
         {
-          layer: 'amnnma',
-          opacity: 1
-        }
+          layer: "amnnma",
+          opacity: 1,
+        },
       ],
       onChapterExit: [
         {
-          layer: 'amnnma',
-          opacity: 0
-        }
-      ]
-    }
-  ]
+          layer: "amnnma",
+          opacity: 0,
+        },
+      ],
+    },
+  ],
 };
